dfbfc5fa83921638a79b55c9a03915c6de4b849d
geraldimanuel <geraldimanuelwijaya@gmail.com>
2023-09-13 22:58:19 +0700

    fix: :bug: aaaaaaaaa

--- routes/web.php
+++ routes/web.php
@@ -46,7 +46,7 @@ Route::middleware('guest')->controller(ResetPasswordController::class)->group(fu
     Route::get('/forgot-password', 'index')->name('password.request');
     Route::post('/forgot-password', 'forgot_password')->name('password.email');
     Route::get('/reset-password/{token}', 'reset_token')->name('password.reset');
-    Route::post('/reset-password', 'reset')->name('passord.update');;
+    Route::post('/reset-password', 'reset')->name('passord.update');
 });
 
 Route::controller(MerchController::class)->group(function () {
