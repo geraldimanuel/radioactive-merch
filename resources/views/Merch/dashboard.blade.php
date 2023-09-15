db20565d3e9a636d468d8decf9a2eab1a6c94b8c
VgTan <alfonsusvega9@gmail.com>
2023-09-15 11:35:45 +0700

    feat: :rocket: dashboard
    amin

--- resources/views/Merch/dashboard.blade.php
+++ resources/views/Merch/dashboard.blade.php
@@ -3,59 +3,91 @@
 
 <head>
     <title>Dashboard</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    @vite('resources/css/app.css')
 </head>
 
 <body>
-    <h1>Orders</h1>
-    <table border="1" cellpadding="10">
-        <tr>
-            <th>No</th>
-            <th>Order ID</th>
-            <th>Name</th>
-            <th>Items</th>
-            <th>Total Price</th>
-            <th>Status</th>
-        </tr>
-        <?php $no = 1; ?>
-        @foreach ($orders as $order)
-            <tr>
-                <td>{{ $no++ }}</td>
-                <td>{{ $order->id }}</td>
-                <td>{{ $order->name }}</td>
-                <td>
-                    <ul>
-                        @foreach ($detailTrans as $obj)
+    <div class="container flex justify-center align-middle items-center">
+        <div class="">
+            <h1 class="font-taruno">Orders</h1>
+            <table border="1" class="h-full" cellpadding="10">
+                <tr>
+                    <th>No</th>
+                    <th>Order ID</th>
+                    <th>Name</th>
+                    <th>Items</th>
+                    <th>Total Price</th>
+                    <th>Bukti Transfer</th>
+                    <th>Status</th>
+                </tr>
+                <?php $no = 1; ?>
+                @foreach ($orders as $order)
+                <tr>
+                    <td>{{ $no++ }}</td>
+                    <td>{{ $order->id }}</td>
+                    <td>{{ $order->name }}</td>
+                    <td>
+                        <ul>
+                            @foreach ($detailTrans as $obj)
                             @if ($obj->order_id == $order->id)
-                                <li>
-                                    @foreach ($merchs as $merch)
-                                        @if ($merch->id == $obj->merch_id)
-                                            {{ $merch->name }} ({{ $obj->qty }})
-                                        @endif
-                                    @endforeach
-                                </li>
+                            <li>
+                                @foreach ($merchs as $merch)
+                                @if ($merch->id == $obj->merch_id)
+                                {{ $merch->name }} ({{ $obj->qty }})
+                                @endif
+                                @endforeach
+                            </li>
                             @endif
-                        @endforeach
-                    </ul>
-                </td>
-                <td>
-                    @foreach ($merchs as $merch)
+                            @endforeach
+                        </ul>
+                    </td>
+                    <td>
+                        @foreach ($merchs as $merch)
                         @if ($merch->id == $obj->merch_id)
-                        <img class="w-2 object-cover" src="/images/{{ $merch->description }}//{{ $merch->image1 }}"/>
+                        <img class="w-[150px] h-full object-cover cursor-pointer"
+                            src="/images/{{ $merch->description }}//{{ $merch->image1 }}" alt="Flower"
+                            onclick="showModal('/images/{{ $merch->description }}//{{ $merch->image1 }}')" />
                         @endif
-                    @endforeach
-                </td>
-                <td>{{ $order->total_price }}</td>
-                <td>
-                    <select required='required' class="form-control" name='status'>
-                        <optgroup label={{ $order->status }} />
-                        <option value='Unpaid'>Unpaid</option>
-                        <option value='Paid'>Paid</option>
-                        <option value='Cancelled'>Cancelled</option>
-                    </select>
-                </td>
-            </tr>
-        @endforeach
-</table>
+                        <div id="modal"
+                            class="hidden fixed top-0 left-0 z-80 w-screen h-screen bg-black/70 flex justify-center items-center">
+
+                            <a class="fixed z-90 top-6 right-8 text-white text-5xl font-bold" href="javascript:void(0)"
+                                onclick="closeModal()">&times;</a>
+
+                            <img id="modal-img" class="max-w-[800px] max-h-[600px] object-cover" />
+                        </div>
+                        @endforeach
+                    </td>
+                    <td>{{ $order->total_price }}</td>
+                    <td>
+                        <select required='required' class="form-control" name='status'>
+                            <optgroup label={{ $order->status }} />
+                            <option value='Unpaid'>Unpaid</option>
+                            <option value='Paid'>Paid</option>
+                            <option value='Cancelled'>Cancelled</option>
+                        </select>
+                    </td>
+                </tr>
+                @endforeach
+            </table>
+        </div>
+    </div>
+    <script>
+    var modal = document.getElementById("modal");
+
+    var modalImg = document.getElementById("modal-img");
+
+    function showModal(src) {
+        modal.classList.remove('hidden');
+        modalImg.src = src;
+    }
+
+    function closeModal() {
+        modal.classList.add('hidden');
+    }
+    </script>
+
 </body>
 
 </html>
